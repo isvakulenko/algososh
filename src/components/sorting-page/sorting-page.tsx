ffa989a081c978b553a0018bf17ea6c3f8392558
isvakulenko <ivan-vakulenko@yandex.ru>
2022-10-26 00:18:04 +0300

    Add changing state for column

--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -10,15 +10,16 @@ import { Direction } from "../../types/direction";
 import { ElementStates } from "../../types/element-states";
 
 type TColumn = {
-  number: number;
-  state: ElementStates;
+  renderArray: number[];
+  leftIndex?: number;
+  rightIndex?: number;
 };
 
 export const SortingPage: React.FC = () => {
   const [sortingType, setSortingType] = useState<string>("select");
   const [sortDirection, setSortDirection] = useState<Direction>();
-  const [initialArr, setInitialArr] = useState<Array<TColumn>>([]);
-  const [sortStepArr, setSortStepArr] = useState<Array<Array<TColumn>>>([]);
+  const [initialArr, setInitialArr] = useState<Array<number>>([]);
+  const [sortStepArr, setSortStepArr] = useState<Array<TColumn>>([]);
   // Номер текущего шага
   const [currentStep, setCurrentStep] = useState(0);
   const [isExecute, setIsExecute] = useState(false);
@@ -38,16 +39,13 @@ export const SortingPage: React.FC = () => {
     for (let i = 0; i <= arrLen - 1; i++) {
       //Случайное число для массива
       let randNum = Math.floor(Math.random() * (maxNum - minNum)) + minNum;
-      arr.push({
-        number: randNum,
-        state: ElementStates.Default,
-      });
+      arr.push(randNum);
     }
     return arr;
   };
   //Функция для выполнения перестановок в массиве
   const swap = (
-    arr: TColumn[],
+    arr: number[],
     firstIndex: number,
     secondIndex: number
   ): void => {
@@ -68,64 +66,78 @@ export const SortingPage: React.FC = () => {
 
   const startSortArr = (sortDirection: Direction) => {
     setIsExecute(true);
-    const steps = (
+    let steps = (
       sortingType === "select" ? getSelectSortSteps : getBubbleSortSteps
     )(initialArr, sortDirection);
     setSortStepArr(steps);
     setSortDirection(sortDirection);
-    // //Сбросим счетчик
+    //Сбросим счетчик
     setCurrentStep(0);
     stepTimer(steps);
   };
 
-  const getBubbleSortSteps = (arr: TColumn[], direction: Direction) => {
-    console.log("arrStart", arr);
-    let sortState: TColumn[][] = [[...arr]];
-    let step = 1;
+  const getBubbleSortSteps = (
+    arr: number[],
+    direction: Direction
+  ): TColumn[] => {
+    //сюда будем вставлять промежуточные результаты сортировки
+    const sortStep: TColumn[] = [];
     for (let i = 0; i < arr.length; i++) {
       let swapped = false;
       for (let j = 0; j < arr.length - i - 1; j++) {
         if (
           direction === Direction.Ascending
-            ? arr[j].number > arr[j + 1].number
-            : arr[j].number < arr[j + 1].number
+            ? arr[j] > arr[j + 1]
+            : arr[j] < arr[j + 1]
         ) {
           swap(arr, j, j + 1);
-          sortState.push([...arr]);
-          step++;
           swapped = true;
         }
+        // после каждого случая перестановки чисел записываем промежуточные
+        //массивы в объект
+        sortStep.push({
+          renderArray: [...arr],
+          leftIndex: j,
+          rightIndex: j + 1,
+        });
       }
       if (!swapped) {
         break;
       }
     }
-    return sortState;
+    return sortStep;
   };
 
-  const getSelectSortSteps = (arr: TColumn[], direction: Direction) => {
-    let sortState: TColumn[][] = [[...arr]];
+  const getSelectSortSteps = (arr: number[], direction: Direction) => {
+    //сюда будем вставлять промежуточные результаты сортировки
+    const sortStep: TColumn[] = [];
     const { length } = arr;
     for (let i = 0; i < length - 1; i++) {
       let minInd = i;
       for (let k = i + 1; k < length; k++) {
         if (
           direction === Direction.Ascending
-            ? arr[k].number < arr[minInd].number
-            : arr[k].number > arr[minInd].number
+            ? arr[k] < arr[minInd]
+            : arr[k] > arr[minInd]
         ) {
           minInd = k;
         }
       }
       if (minInd !== i) {
         swap(arr, i, minInd);
-        sortState.push([...arr]);
-      }
+        // после каждого случая перестановки чисел записываем промежуточные
+        //массивы в объект
+        sortStep.push({
+          renderArray: [...arr],
+          leftIndex: i,
+          rightIndex: minInd,
+        });
+         }
     }
-    return sortState;
+    return sortStep;
   };
   // Вызовем таймер, увеличивающий шаг каждую секунду
-  const stepTimer = (steps: TColumn[][]) => {
+  const stepTimer = (steps: TColumn[]) => {
     if (steps.length > 1) {
       //Счетчик шагов будет увеличиваться на 1 каждые 1000 мс
       let timerId = setInterval(() => {
@@ -140,6 +152,21 @@ export const SortingPage: React.FC = () => {
       }, SHORT_DELAY_IN_MS);
     }
   };
+  // Функция отвечает за цвет при анимировании перехода
+  const getElementsState = (
+    index: number,
+    currentRenderArray: TColumn
+  ): ElementStates => {
+    if (
+      [currentRenderArray.leftIndex, currentRenderArray.rightIndex].includes(
+        index
+      )
+    ) {
+      return ElementStates.Changing;
+    }
+    return ElementStates.Default;
+  };
+
   return (
     <SolutionLayout title="Сортировка массива">
       <div className={styles.wrapper}>
@@ -182,14 +209,22 @@ export const SortingPage: React.FC = () => {
       </div>
       <div>
         <ul className={styles.list}>
-          {(sortStepArr || initialArr) &&
-            (sortStepArr[currentStep] || initialArr).map((item, index) => {
+          {(sortStepArr[currentStep]?.renderArray || initialArr).map(
+            (item, index) => {
               return (
                 <li key={index}>
-                  <Column index={item.number} state={item.state} />
+                  <Column
+                    index={item}
+                    state={
+                      isExecute
+                        ? getElementsState(index, sortStepArr[currentStep])
+                        : ElementStates.Default
+                    }
+                  />
                 </li>
               );
-            })}
+            }
+          )}
         </ul>
       </div>
     </SolutionLayout>
