c1007048ca308c9c30f084b0fcaa9910eaf6fee0
isvakulenko <ivan-vakulenko@yandex.ru>
2022-11-15 21:44:54 +0300

    Add fibonacci page  cy test

---
+++ cypress/e2e/fibonacci-tests.cy.js
@@ -0,0 +1,30 @@
+import { getFibonacciNumbersArr } from "../../src/components/fibonacci-page/utils";
+import { SHORT_DELAY_IN_MS } from "../../src/constants/delays";
+
+describe("Page with Fibonacci numbers app is right", () => {
+  before(function () {
+    cy.visit("http://localhost:3000/fibonacci");
+  });
+  // Проверьте, что если в инпуте пусто, то кнопка
+  // добавления недоступна.
+  it("should disable button if input is empty", () => {
+    if (cy.get("input").should("have.value", "")) {
+      cy.contains("Рассчитать").as("button");
+      cy.get("@button").should("be.disabled");
+    }
+  });
+  // Проверьте, что числа генерируются корректно.
+  it("should Fibonacci numbers get correctly", () => {
+    cy.get("input").type("19");
+    cy.get("input").should("have.value", "19");
+    cy.get("button").contains("Рассчитать").click();
+// Максимальное расчетное число чисел Фибоначчи - 19
+    const FibonacciNumbersArr = getFibonacciNumbersArr(19);
+// Дадим задержку на вычисление всех чисел Фибоначчи
+    cy.wait(19*SHORT_DELAY_IN_MS );
+      cy.get("[class*=circle_content]").each(($number, index) => {
+        cy.wrap($number).contains(FibonacciNumbersArr[index]);
+       }
+    );
+    });
+});
